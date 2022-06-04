da6bbbf837624b7a314cd85701e5ad6947dd7f04
atlan-antillia <atlan@antillia.com>
2022-06-04 16:45:54 +0900

    Removed japanese_roadsigns*.bat

--- japanese_roadsigns_detect.bat
+++
@@ -1,8 +0,0 @@
-python model_inspect.py ^
-  --runmode=saved_model_infer ^
-  --model_name=efficientdet-d0 ^
-  --saved_model_dir=./projects/Japanese_RoadSigns/saved_model ^
-  --min_score_thresh=0.2 ^
-  --hparams="label_map=./projects/Japanese_RoadSigns/label_map.yaml" ^
-  --input_image=./projects/Japanese_RoadSigns/realistic_test_dataset/*.jpg ^
-  --output_image_dir=./projects/Japanese_RoadSigns/realistic_test_dataset_outputs
